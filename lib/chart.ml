6b82c32d0ee57d7aaa1a66bb83ca30f269fad41a
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-05-15 10:27:29 +0100

    Add history for counting

--- lib/chart.ml
+++ lib/chart.ml
@@ -1,16 +1,41 @@
 open Core
 
-type t = Set.M(Edge).t * Terminal.t array * Production_rules.t * Non_terminal.t
+module X = struct
+  module Y = struct
+    type t = Edge.t list [@@deriving compare, sexp]
+  end
+
+  include Y
+  include Comparator.Make (Y)
+end
+
+module W = struct
+  module Z = struct
+    type t = Set.M(X).t [@@deriving compare, sexp]
+  end
+
+  include Z
+  include Comparator.Make (Z)
+end
+
+type t =
+  Set.M(Edge).t
+  * Terminal.t array
+  * Production_rules.t
+  * Non_terminal.t
+  * W.t Map.M(Edge).t
 [@@deriving sexp]
 
 let create sentence production_rules (((end_non_terminal, _), _) as start_edge) =
-  ( Set.add (Set.empty (module Edge)) start_edge
-  , sentence
-  , production_rules
-  , end_non_terminal )
+  (( Set.add (Set.empty (module Edge)) start_edge
+   , sentence
+   , production_rules
+   , end_non_terminal
+   , Map.empty (module Edge) )
+    :> t)
 ;;
 
-let predict (set, sentence, production_rules, end_non_terminal) =
+let predict (set, sentence, production_rules, end_non_terminal, histories) =
   ( Set.fold set ~init:set ~f:(fun current_set ((_, (_, postfix_string)), (_, j)) ->
         match postfix_string with
         | Meta_symbol.NT non_terminal_1 :: _ ->
@@ -22,56 +47,93 @@ let predict (set, sentence, production_rules, end_non_terminal) =
         | _ -> current_set)
   , sentence
   , production_rules
-  , end_non_terminal )
+  , end_non_terminal
+  , histories )
 ;;
 
-let scan (set, sentence, production_rules, end_non_terminal) =
-  ( Set.fold
+let scan (set, sentence, production_rules, end_non_terminal, histories) =
+  let new_set, new_histories =
+    Set.fold
       set
-      ~init:set
-      ~f:(fun current_set ((non_terminal, (prefix_string, postfix_string)), (i, j)) ->
+      ~init:(set, histories)
+      ~f:(fun
+           (current_set, current_histories)
+           (((non_terminal, (prefix_string, postfix_string)), (i, j)) as edge)
+         ->
         match prefix_string, postfix_string with
         | [], Meta_symbol.T terminal :: postfix_string_remainder ->
           if j < Array.length sentence && Terminal.compare sentence.(j) terminal = 0
-          then
-            Set.add
-              current_set
+          then (
+            let new_edge =
               ( (non_terminal, ([ Meta_symbol.T terminal ], postfix_string_remainder))
               , (i, j + 1) )
-          else current_set
-        | _ -> current_set)
-  , sentence
-  , production_rules
-  , end_non_terminal )
+            in
+            ( Set.add current_set new_edge
+            , Map.set
+                current_histories
+                ~key:new_edge
+                ~data:
+                  (match Map.find current_histories (edge :> Edge.t) with
+                  | Some history_set -> history_set
+                  | None -> Set.singleton (module X) []) ))
+          else current_set, current_histories
+        | _ -> current_set, current_histories)
+  in
+  new_set, sentence, production_rules, end_non_terminal, new_histories
 ;;
 
-let complete (set, sentence, production_rules, end_non_terminal) =
-  ( Set.fold
+let complete (set, sentence, production_rules, end_non_terminal, histories) =
+  let new_set, new_histories =
+    Set.fold
       set
-      ~init:set
-      ~f:(fun set_1 ((non_terminal_1, (prefix_string, postfix_string_1)), (i, k_1)) ->
+      ~init:(set, histories)
+      ~f:(fun
+           (set_1, histories_1)
+           (((non_terminal_1, (prefix_string, postfix_string_1)), (i, k_1)) as edge_1)
+         ->
         match postfix_string_1 with
         | Meta_symbol.NT non_terminal_2 :: postfix_string_remainder ->
           Set.fold
             set_1
-            ~init:set_1
-            ~f:(fun set_2 ((non_terminal_3, (_, postfix_string_2)), (k_2, j)) ->
+            ~init:(set_1, histories_1)
+            ~f:(fun
+                 (set_2, histories_2)
+                 (((non_terminal_3, (_, postfix_string_2)), (k_2, j)) as edge_2)
+               ->
               match postfix_string_2 with
               | [] ->
                 if k_1 = k_2 && Non_terminal.compare non_terminal_2 non_terminal_3 = 0
-                then
-                  Set.add
-                    set_2
+                then (
+                  let edge_3 =
                     ( ( non_terminal_1
                       , ( prefix_string @ [ Meta_symbol.NT non_terminal_2 ]
                         , postfix_string_remainder ) )
                     , (i, j) )
-                else set_2
-              | _ -> set_2)
-        | _ -> set_1)
-  , sentence
-  , production_rules
-  , end_non_terminal )
+                  in
+                  ( Set.add set_2 edge_3
+                  , Map.set
+                      histories_2
+                      ~key:edge_3
+                      ~data:
+                        (let edge_1_paths =
+                           match Map.find histories_2 edge_1 with
+                           | Some history_set -> history_set
+                           | None -> Set.singleton (module X) []
+                         in
+                         let new_history_paths =
+                           Set.map
+                             (module X)
+                             edge_1_paths
+                             ~f:(fun edge_1_path -> edge_2 :: edge_1_path)
+                         in
+                         match Map.find histories_2 edge_3 with
+                         | Some history_set -> Set.union history_set new_history_paths
+                         | None -> new_history_paths) ))
+                else set_2, histories_2
+              | _ -> set_2, histories_2)
+        | _ -> set_1, histories_1)
+  in
+  new_set, sentence, production_rules, end_non_terminal, new_histories
 ;;
 
 let update chart : t = complete (scan (predict chart))
@@ -79,19 +141,26 @@ let update chart : t = complete (scan (predict chart))
 let fill_chart chart =
   let rec fill_chart_util chart last_length =
     let new_chart = update chart in
-    let set, _, _, _ = new_chart in
+    let set, _, _, _, _ = new_chart in
     let new_length = Set.length set in
     if new_length = last_length then new_chart else fill_chart_util new_chart new_length
   in
   fill_chart_util chart 0
 ;;
 
-let count_parses (set, sentence, _, end_non_terminal) =
-  Set.count set ~f:(fun ((non_terminal, (_, postfix_string)), (i, j)) ->
-      match postfix_string with
-      | [] ->
-        i = 0
-        && j = Array.length sentence
-        && Non_terminal.compare non_terminal end_non_terminal = 0
-      | _ -> false)
+let count_parses (set, sentence, _, end_non_terminal, histories) =
+  match
+    Set.find set ~f:(fun ((non_terminal, (_, postfix_string)), (i, j)) ->
+        match postfix_string with
+        | [] ->
+          i = 0
+          && j = Array.length sentence
+          && Non_terminal.compare non_terminal end_non_terminal = 0
+        | _ -> false)
+  with
+  | None -> 0
+  | Some edge ->
+    (match Map.find histories edge with
+    | None -> 0
+    | Some paths -> Set.length paths)
 ;;
